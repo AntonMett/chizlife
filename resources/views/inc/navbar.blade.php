3198acb27a96af71124255ab3d53f0c24646adf1
Anton Mett <antonmett@gmail.com>
2021-08-11 01:46:45 +0300

    trying to fix mobile

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-dark navbar-expand-lg fixed-top" aria-label="Main">
-    <div class="container">
-        <a href="/" class="navbar-brand d-inline-flex text-uppercase">ChizGaming
+    <div class="container-fluid">
+        <a href="/" class="navbar-brand d-inline-flex">ChizGaming
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
-            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="material-icons icon-2x" aria-hidden="true">Menu</i>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse text-uppercase" id="navbar">
-            <ul class="navbar-nav ms-auto">
+        <div class="collapse navbar-collapse" id="navbarToggler">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a href="/" class="nav-link">Home</a>
                 </li>
