c61bd4efbe02438ba3f70ae2d82a1814c3308b60
Anton Mett <antonmett@gmail.com>
2021-08-16 12:40:17 +0300

    source opens new tab now and added text to readme file

--- resources/views/inc/hero.blade.php
+++ resources/views/inc/hero.blade.php
@@ -7,7 +7,8 @@
             in my database and will be visible in All Contact Messages page. There You can Update or Delete your
             messages from database. Also you can check out source code in my github -
         <div class="d-flex justify-content-center ">
-            <a href="https://github.com/AntonMett/chizlife"><button class="btn btn-info">Source</button></a>
+            <a href="https://github.com/AntonMett/chizlife" target="_blank"><button
+                    class="btn btn-info">Source</button></a>
         </div>
         </p>
     </div>
