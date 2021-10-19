c37643689f8645ec356e22bbfc33be809ef1e9b5
Anton <antonmett@gmail.com>
2021-10-19 12:17:37 +0300

    changes to hero

--- resources/views/inc/hero.blade.php
+++ resources/views/inc/hero.blade.php
@@ -1,15 +1,25 @@
 <div class="jumbotron">
-    <div class="container">
-        <h1 class="d-flex justify-content-center">Hello Everyone!</h1>
+    <div class="container text-center">
+        <h1>Hello Everyone!</h1>
         <p>This is my Laravel testing and learning website! Feel free to test out Contact page,
             be sure to fill out form correctly or
             you will be given some alert messages ;) After you submit your contact request your message will be stored
             in my database and will be visible in All Contact Messages page. There You can Update or Delete your
-            messages from database. Also you can check out source code in my github -
+            messages from database.<br>
+        <h2>Public API</h2><br>
+            GET api/products - Get all products from database, limit 1000, default status_id=1, optional filters<br>
+            GET api/products/{product} - Get selected product<br>
+            GET api/register - Register New user and get api token. Required name, email, password<br><br>
+            <h3>Private API - pass your token as a bearer token in request</h3><br>
+            POST api/products - Add new product to database. Required: name, price, status_id, external_id.<br>
+            PUT api/products/{product} - Update selected product. Required: name, price, status_id, external_id.<br>
+            DELETE api/products/{product} - Delete selected product. Required: name, price, status_id, external_id.<br><br>
+
         <div class="d-flex justify-content-center ">
             <a href="https://github.com/AntonMett/chizlife" target="_blank"><button
-                    class="btn btn-info">Source</button></a>
+                    class="btn btn-info">CHECK CODE ON GITHUB</button></a>
         </div>
         </p>
     </div>
 </div>
+php
